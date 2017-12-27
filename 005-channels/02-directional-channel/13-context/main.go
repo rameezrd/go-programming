a127ed20da8bdd4da5f20b717e32eb7685fa75d0
Rameez Deshmukh <rameezrd@gmail.com>
2017-12-27 13:03:05 +0100

    context

---
+++ 005-channels/02-directional-channel/13-context/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	fmt.Println("ctx chk:\t", ctx.Err())
+	fmt.Println("Goroutine chk 1:\t", runtime.NumGoroutine())
+
+	go func() {
+		n := 0
+		for {
+			select {
+			case <-ctx.Done():
+				return
+
+			default:
+				n++
+				time.Sleep(time.Millisecond * 200)
+				fmt.Println("working: ", n)
+			}
+		}
+	}()
+	time.Sleep(time.Second * 2)
+	fmt.Println("ctx chk2:\t", ctx.Err())
+	fmt.Println("Goroutine chk:2\t", runtime.NumGoroutine())
+	fmt.Println("about to cancel")
+	cancel()
+	fmt.Println("Context cancelled")
+
+	time.Sleep(time.Second * 2)
+	fmt.Println("ctx chk2:\t", ctx.Err())
+	fmt.Println("Goroutine chk:2\t", runtime.NumGoroutine())
+
+}

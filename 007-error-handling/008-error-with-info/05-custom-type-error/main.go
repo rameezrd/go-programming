c1e892e574209e61ac4b5b059828d098e51a8263
Rameez Deshmukh <rameezrd@gmail.com>
2017-12-28 20:18:03 +0100

    error habdling

---
+++ 007-error-handling/008-error-with-info/05-custom-type-error/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type norgateMathError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (n norgateMathError) Error() string {
+	return fmt.Sprintf("a norgate map error occured: %v %v %v", n.lat, n.long, n.err)
+}
+func main() {
+	_, err := sqrt(-10.23)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+func sqrt(f float64) (float64, error) {
+	if f < 0 {
+		nme := fmt.Errorf("norgate math redux: square root of negative number: %v", f)
+		return 0, norgateMathError{"50.2289 n", "99.4656 w", nme}
+	}
+	return 42, nil
+}

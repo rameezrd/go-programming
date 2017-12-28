16ec2a173f360452d98fcdb38bedea71b9f5bcd3
Rameez Deshmukh <rameezrd@gmail.com>
2017-12-28 11:44:20 +0100

    error handling

---
+++ 007-error-handling/01-checking-error/02-create/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	f, err := os.Create("name.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	r := strings.NewReader("James Bond")
+	io.Copy(f, r)
+}
